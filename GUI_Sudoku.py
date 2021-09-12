7fb81e3f1889ac533c1cc29c1a1b3a223fa43291
Kyle Soares <kyle.m.soares@gmail.com>
2021-09-12 15:29:27 -0500

    create GUI file

---
+++ GUI_Sudoku.py
@@ -0,0 +1,53 @@
+import pygame
+from solver import solve, valid
+import time
+pygame.font.init()
+
+
+class Grid:
+    # default board
+    board = [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
+
+
+    # __init__
+
+    # update
+
+    # place
+
+    # sketch
+
+    # draw
+
+    # select
+
+    # clear
+
+    # click
+
+    # is_finished
+
+class Cube:
+
+    # __init__
+
+    # draw
+
+# redraw_window
+
+# format_time
+
+def main()
+
+#main()
+#pygame.quit
