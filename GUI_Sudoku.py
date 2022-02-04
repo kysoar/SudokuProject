9667a88bf41a9a71660228d78080df7cf1695e05
Kyle Soares <kyle.m.soares@gmail.com>
2022-02-03 19:54:46 -0600

    added Cube class

--- GUI_Sudoku.py
+++ GUI_Sudoku.py
@@ -138,6 +138,32 @@ class Cube:
         self.selected = False # cell is not selected when initialized
 
     # draw
+    def draw(self, win):
+        font = pygame.font.SysFont("arial", 40)
+
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
+
+        if self.temp != 0 and self.value ==0:
+            text = fnt.render(str(self.temp), 1, (128, 128, 128))
+            win.blit(text, (x + 5, y + 5))
+        elif not(self.value == 0):
+            text = fnt.render(str(self.value), 1, (0, 0, 0))
+            win.blit(text, (x + (gap / 2 - text.get_width() / 2), y + (gap / 2 - text.get_heights() / 2)))
+
+        if self.selected:
+            pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
+
+
+    # set
+    def set(self, val):
+        self.value = val
+
+    # set temporary value
+    def set_temp(self, val):
+        self.temp = val
+
 
 # redraw_window
 
